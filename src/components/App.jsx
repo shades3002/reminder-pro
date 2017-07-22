7f09fec4e44683158e4dbb506afe0bfa62b43569
shades3002 <shades3002@gmail.com>
2017-07-21 23:51:42 -0400

    Mapping Dispatch To Props and Binding Action Creators

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-import '../App.css';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+    }
+
+    addReminder() {
+        this.props.addReminder(this.state.text);
+    }
+
     render() {
         return (        
             <div className="App">
@@ -13,11 +25,13 @@ class App extends Component {
                         <input 
                             className="form-control"
                             placeholder="I have to..."
+                            onChange={event => this.setState({text: event.target.value})}
                         />
                     </div>
                     <button
                         type="button"
                         className="btn btn-success"
+                        onClick={() => this.addReminder()}
                     >
                         Add Reminder
                     </button>
@@ -27,4 +41,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(null, {addReminder})(App);
